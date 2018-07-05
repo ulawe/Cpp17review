c0417278ab9f0311b5a6769f5d21a0692f7e53fc
Dorin IACOB <diacob2@math.uic.edu>
2018-07-05 11:54:22 -0500

    intermediate

--- Recapitulare/MyApp.cpp
+++ Recapitulare/MyApp.cpp
@@ -1,88 +1,125 @@
-﻿/*** Hello World Compile and Run ***/
+/*** Hello World Compile and Run ***/
 
 #include <iostream>
 #include <cstdint>
 #include <string>
+#include <tuple>
+#include <functional>
+#include <bitset>
+
+#include <cstddef>
+#include <stdexcept>
+#include <cstring>
+#include <ostream>
 using namespace std;
 
+class MyRectangle{
+public:
+	enum Color:unsigned short int{ Infrarosu, Rosu, Oranj, Galben, Verde, Albastru, Indigo, Violet, Ultraviolet };
+	int x, y;
+	int getArea();
+	MyRectangle(initializer_list<int>);
+};
+int MyRectangle::getArea(){ return x*y; }
+MyRectangle::MyRectangle(initializer_list<int> args){
+	decltype(auto) li = args.begin();
+	x = *li++;
+	y = *li;
+}
+
+tuple<int, double, char> getTuple(){ return {1, 1.2, 'b'}; }
+
+void call(function<void()> f){ f(); }
+
+union Mix{
+	char c[4];                 // 4 bytes
+	struct{ short lo, hi; } s;  // 4 bytes
+	int i;                     // 4 bytes
+} m;
+
+class MyNum{
+public:
+	int value;
+	//explicit MyNum(int i):value(i){};
+	operator int(){ return value; };
+};
+
+
+
+#ifndef _MSC_VER
+#  if __cplusplus < 201103
+#    define CONSTEXPR11_TN
+#    define CONSTEXPR14_TN
+#    define NOEXCEPT_TN
+#  elif __cplusplus < 201402
+#    define CONSTEXPR11_TN constexpr
+#    define CONSTEXPR14_TN
+#    define NOEXCEPT_TN noexcept
+#  else
+#    define CONSTEXPR11_TN constexpr
+#    define CONSTEXPR14_TN constexpr
+#    define NOEXCEPT_TN noexcept
+#  endif
+#else  // _MSC_VER
+#  if _MSC_VER < 1900
+#    define CONSTEXPR11_TN
+#    define CONSTEXPR14_TN
+#    define NOEXCEPT_TN
+#  elif _MSC_VER < 2000
+#    define CONSTEXPR11_TN constexpr
+#    define CONSTEXPR14_TN
+#    define NOEXCEPT_TN noexcept
+#  else
+#    define CONSTEXPR11_TN constexpr
+#    define CONSTEXPR14_TN constexpr
+#    define NOEXCEPT_TN noexcept
+#  endif
+#endif  // _MSC_VER
+inline std::ostream& operator<<(std::ostream& os, string_view const& s){
+	return os.write(s.data(), s.size());
+}
+template <class T> constexpr std::string_view type_name(){
+	using namespace std;
+#ifdef __clang__
+	string_view p = __PRETTY_FUNCTION__;
+	return string_view(p.data()+34, p.size()-34-1);
+#elif defined(__GNUC__)
+	string_view p = __PRETTY_FUNCTION__;
+#  if __cplusplus < 201402
+	return string_view(p.data()+36, p.size()-36-1);
+#  else
+	return string_view(p.data()+49, p.find(';', 49)-49);
+#  endif
+#elif defined(_MSC_VER)
+	string_view p = __FUNCSIG__;
+	return string_view(p.data()+84, p.size()-84-7);
+#endif
+}
+
+
+
 int main(){
-
-	int x(1), y(2);
-	char      myChar = 0;  // -128   to +127
-	short     myShort = 0; // -32768 to +32767
-	int       myInt = 0;   // -2^31  to +2^31-1
-	long      myLong = 0;  // -2^31  to +2^31-1
-	long long myL2 = 0;    // -2^63 to +2^63-1
-
-	cout<<sizeof(myChar)<<endl // 1 byte (per definition)
-		<<sizeof(myShort)<<endl  // 2
-		<<sizeof(myInt)<<endl    // 4
-		<<sizeof(myLong)<<endl   // 4
-		<<sizeof(myL2)<<endl;    // 8
-
-	cout<<"Hello World!"<<endl;
-	cout<<x<<y<<endl; //"12"
-
-	int8_t   myInt8 = 0; // 8 bits
-	int16_t  myInt16 = 0; // 16 bits
-	int32_t  myInt32 = 0; // 32 bits
-	int64_t  myInt64 = 0; // 64 bits
-
-	signed char  smyChar = 0; // -128   to +127
-	signed short smyShort = 0; // -32768 to +32767
-	signed int   smyInt = 0; // -2^31  to +2^31-1
-	signed long  smyLong = 0; // -2^31  to +2^31-1
-	signed long long smyL2 = 0; // -2^63  to +2^63-1
-
-	unsigned char  umyChar = 0; // 0 to 255
-	unsigned short umyShort = 0; // 0 to 32767
-	unsigned int   umyInt = 0; // 0 to 2^32-1
-	unsigned long  umyLong = 0; // 0 to 2^32-1
-	unsigned long long umyL2 = 0; // 0 to 2^64-1
-
-	unsigned uInt(0); // unsigned int
-	signed sInt(1);   // signed int
-
-											// Floating-point types
-	float  myFloat = 3.14F; // 3.4E +/- 38 (7 digits)
-	double myDouble = 3.14; // 1.7E +/- 308 (15 digits)
-	long double myLongDouble = 3.14; // same as double
-
-	myFloat = 12345.678F; // rounded to 12345.68
-	cout<<myFloat<<endl;
-	myFloat = 3E2; // 3*10^2 = 300
-	cout<<"new myFloat:"<<myFloat<<endl;
-
-								 // Char type
-	char c = 'x'; // assigns 120 (ASCII for 'x')
-	cout<<c; // prints 'x'
-
-	int i = c;            // assigns 120
-	cout<<i;       // prints 120
-	cout<<(char) i<<endl; // prints 'x'
-
-	char ascii =u8'x'; // use UTF-8 encoding
-	cout << ascii<<endl;
-											// Bool type
-	bool b = false; // true or false value
-
-									// String encodings
-	wstring s1 = L"Hello";
-	const wchar_t* s2 = L"Hello";
-	string s3 = "Compiler-defined encoding";
-	string s4 = u8"UTF-8 string";
-	u16string s5 = u"UTF-16 string";
-	u32string s6 = U"UTF-32 string";
-	string s7 = u8"An asterisk: \u002A";
-	cout<<s7<<endl;
-	string s = "Hello \
-World!";
-	cout<<s<<endl;
-	s = "\tHello\rWorld";
-	cout<<s<<endl;
-	s = R"(Hello World -\_0_/-
-               |
-	      /*\)";
-	cout<<s<<endl;
+	auto&&[i, d, c] = getTuple();
+	cout<<i<<" "<<c<<endl;
+	cout<<type_name<decltype(i)>()<<" "<<type_name<decltype(c)>()<<endl;
+	int ii(i);
+	[=, b = 2]() mutable {b++; ii += b; }();
+	cout<<ii<<endl;
+	MyRectangle r{2, 6};
+	cout<<r.x<<" "<<r.y<<" "<<r.getArea()<<endl;
+	MyRectangle::Color colour = MyRectangle::Color::Rosu;
+	cout<<to_string(colour)<<endl;
+	m.i = 0xFF00F00F;
+	cout<<(bitset<32>)m.i<<endl;              // 11111111 00000000 11110000 00001111
+	cout<<(bitset<16>)m.s.hi<<endl;           // 11111111 00000000
+	cout<<(bitset<16>)m.s.lo<<endl;           //                   11110000 00001111
+	cout<<(bitset<8>)m.c[3]<<endl;            // 11111111
+	cout<<(bitset<8>)m.c[2]<<endl;            //          00000000
+	cout<<(bitset<8>)m.c[1]<<endl;            //                   11110000
+	cout<<(bitset<8>)m.c[0]<<endl;            //                            00001111
+	MyNum C = {'H'};
+	MyNum A{5};
+	int iii = A;
+	cout<<iii<<endl;
 	cin.get();
 }
